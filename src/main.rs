c2aefa82e55a782a770682544bac5f4bb9fa88da
Vishal Singh <vishalsingh2376@gmail.com>
2023-10-16 10:41:33 +0530

    gen rand number and compare with guess

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the Number");
-    println!("Please input your guess");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
     let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read input.");
+    loop {
+        println!("Please input your guess");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input.");
 
-    println!("You Guessed : {}", guess);
+        let guess: u32 = guess.trim().parse().expect("Please enter Number");
+
+        println!("You Guessed : {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
 }
